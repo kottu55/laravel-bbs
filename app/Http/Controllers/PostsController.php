2cad6777432ff67631b09d674dd52a38b30e4a5b
kottu55 <kotsuboon@gmail.com>
2020-11-23 03:23:27 +0000

    first commit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -40,4 +40,38 @@ class PostsController extends Controller
             'post' => $post,
         ]);
     }
+
+    public function edit($post_id)
+    {
+        $post = Post::findOrFail($post_id);
+
+        return view('posts.edit', [
+            'post' => $post,
+        ]);
+    }
+
+    public function update($post_id, Request $request)
+    {
+        $params = $request->validate([
+            'title' => 'required|max:50',
+            'body' => 'required|max:2000',
+        ]);
+
+        $post = Post::findOrFail($post_id);
+        $post->fill($params)->save();
+
+        return redirect()->route('posts.show', ['post' => $post]);
+    }
+
+    public function destroy($post_id)
+    {
+        $post = Post::findOrFail($post_id);
+
+        \DB::transaction(function () use ($post) {
+           $post->comments()->delete();
+           $post->delete();
+        });
+
+        return redirect()->route('top');
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,13 +3,63 @@
 @section('content')
     <div class="container mt-4">
         <div class="border p-4">
+            <div class="mb-4 text-right">
+                <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post]) }}">
+                    編集する
+                </a>
+                <form
+                    style="display: inline-block;"
+                    method="POST"
+                    action="{{ route('posts.destroy', ['post' => $post]) }}"
+                >
+                    @csrf
+                    @method('DELETE')
+
+                    <button class="btn btn-danger">削除する</button>
+                </form>
+            </div>
             <h1 class="h5 mb-4">
                 {{ $post->title }}
             </h1>
 
+
+
             <p class="mb-5">
                 {!! nl2br(e($post->body)) !!}
             </p>
+            <form class="mb-4" method="POST" action="{{ route('comments.store') }}">
+                @csrf
+
+                <input
+                    name="post_id"
+                    type="hidden"
+                    value="{{ $post->id }}"
+                >
+
+                <div class="form-group">
+                    <label for="body">
+                        本文
+                    </label>
+
+                    <textarea
+                        id="body"
+                        name="body"
+                        class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
+                        rows="4"
+                    >{{ old('body') }}</textarea>
+                    @if ($errors->has('body'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('body') }}
+                        </div>
+                    @endif
+                </div>
+
+                <div class="mt-4">
+                    <button type="submit" class="btn btn-primary">
+                        コメントする
+                    </button>
+                </div>
+            </form>
 
             <section>
                 <h2 class="h5 mb-4">
